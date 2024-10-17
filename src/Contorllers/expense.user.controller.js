55d50823949ee86b06ac71e0477811344703010e
muthuraj107 <muthurajkpr8@gmail.com>
2024-10-17 10:21:27 +0530

    Add change in contorller

--- src/Contorllers/expense.user.controller.js
+++ src/Contorllers/expense.user.controller.js
@@ -1,12 +1,37 @@
-const jwt = require("jsonwebtoken");
-const users = require("../Models/expense.user.model");
+const users=require('../Models/expense.user.model')
 
-const JWT_SECRET = "thismysecrettoken"; // Change this to a more secure secret in production
+exports.create=(req,res)=>{
+    const{userName,email,password}=req.body;
+    const User=new users({
+        userName,email,password
+    })
 
+    User.save()
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        return res.status(500).send({
+          message: err.message || "something wrong",
+        });
+      });
+}
+
+exports.data = (req, res) => {
+  users
+    .find()
+    .then((user) => res.send(user))
+    .catch((err) => {
+      res.status(400).send({
+        message: err.message || "Something wrong",
+      });
+    });
+};
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
+    // Find user by email
     const user = await users.findOne({ email });
 
     if (!user) {
@@ -18,19 +43,11 @@ exports.login = async (req, res) => {
       return res.status(401).send({ message: "Invalid credentials" });
     }
 
-    // Create JWT token
-    const token = jwt.sign(
-      { id: user._id, userName: user.userName, email: user.email },
-      JWT_SECRET,
-      { expiresIn: "1h" } // Token expires in 1 hour
-    );
-
-    // Respond with user data and token
+    // Login successful, return user data (excluding password)
     res.send({
       id: user._id,
       userName: user.userName,
       email: user.email,
-      token, // Send token to client
     });
   } catch (err) {
     console.error(err);
